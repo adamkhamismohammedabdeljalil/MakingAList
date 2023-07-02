fdc35d9dea95d67995f7d61c4b8801028283bc98
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-07-02 22:23:15 +0200

    feat:add MakingAList project

---
+++ MakingAList/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MakingAList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("insert a number:");
+                int number = int.Parse(Console.ReadLine());
+                list.Add(number);
+
+            }
+            Console.WriteLine("The list is :");
+            foreach (int number in list)
+            {
+                Console.WriteLine(number);
+                
+            }
+
+            //print the list in a reverse order
+            Console.WriteLine("Print the list in a reverse order");
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
